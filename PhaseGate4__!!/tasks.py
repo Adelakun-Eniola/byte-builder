8d7c3310a83cd5ae5cc3e6a0480de61247676a15
boylax23 <eniolaadelakun2005@gmail.com>
2024-10-10 17:47:35 +0100

    'modified'

--- PhaseGate4__!!/tasks.py
+++ PhaseGate4__!!/tasks.py
@@ -1,4 +1,4 @@
-
+'''
 rows = 5
 for index in range(1, rows+1):
     for number in range(1, index+1):
@@ -7,7 +7,6 @@ for index in range(1, rows+1):
         else:
             print(number, end = " ")
     print()
-
 
 
 
@@ -27,7 +26,7 @@ for index in range(1, rows+1):
         else:
             print(number, end = " ")
     print()
-'''
+
 number = 500
 total = 1
 counter =0
@@ -47,5 +46,14 @@ def factorial(number):
     print(product)
 factorial(12)
 '''
-
-
+number = 7
+first_number =0
+second_number = 0
+third_number = 1
+count = 0
+for i in range(number):
+    first_number = second_number
+    second_number = third_number
+    third_number = first_number+second_number
+    count+=1
+    print(third_number, end = " ")
